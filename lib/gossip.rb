e013372b56120432e520dd64924261529738e941
Ruby <bazzani.joan@gmail.com>
2021-02-01 17:21:36 +0100

    done

---
+++ lib/gossip.rb
@@ -0,0 +1,35 @@
+# frozen_string_literal: true
+
+class Gossip
+  attr_accessor :author, :content
+  def initialize(author, content)
+    @author = author
+    @content = content
+  end
+
+  def save
+    CSV.open('./db/gossip.csv', 'ab') do |csv|
+      csv << [@author, @content]
+    end
+  end
+
+  def self.all
+    all_gossips = []
+    CSV.read('./db/gossip.csv').each do |csv_line|
+      all_gossips << Gossip.new(csv_line[0], csv_line[1])
+    end
+    all_gossips
+  end
+
+  def self.find(id)
+    gossips = all
+    gossips[id.to_i]
+  end
+
+  def self.update(new_author, new_content, id)
+    gossips = self.all
+    gossips[id].author = new_author
+    gossips[id].content = new_content
+    CSV.open('db/gossip.csv', 'w') { gossips.each { |gossip| gossip.save }}
+  end
+end
